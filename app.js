53bd41f89db43bb3e1eff9415ecddec66c6377c5
Marquis <Marquis.jayshaun.jarman@gmail.com>
2021-04-27 04:18:06 -0400

    hw push

---
+++ app.js
@@ -0,0 +1,166 @@
+// Function that returns a string representing a cup of green tea
+const prepareTea = () => 'greenTea';
+
+/*
+Given a function (representing the tea type) and number of cups needed, the
+following function returns an array of strings (each representing a cup of
+a specific type of tea).
+*/
+const getTea = (numOfCups) => {
+  const teaCups = [];
+
+  for(let cups = 1; cups <= numOfCups; cups += 1) {
+    const teaCup = prepareTea();
+    teaCups.push(teaCup);
+  }
+  return teaCups;
+};
+
+// Only change code below this line
+const tea4TeamFCC = getTea(40);
+// Only change code above this line
+
+
+
+
+
+// Function that returns a string representing a cup of green tea
+const prepareGreenTea = () => 'greenTea';
+
+// Function that returns a string representing a cup of black tea
+const prepareBlackTea = () => 'blackTea';
+
+/*
+Given a function (representing the tea type) and number of cups needed, the
+following function returns an array of strings (each representing a cup of
+a specific type of tea).
+*/
+const getTea = (prepareTea, numOfCups) => {
+  const teaCups = [];
+
+  for(let cups = 1; cups <= numOfCups; cups += 1) {
+    const teaCup = prepareTea();
+    teaCups.push(teaCup);
+  }
+  return teaCups;
+};
+
+// Only change code below this line
+const tea4GreenTeamFCC = getTea(prepareGreenTea,27);
+const tea4BlackTeamFCC = getTea(prepareBlackTea,13);
+// Only change code above this line
+
+console.log(
+  tea4GreenTeamFCC,
+  tea4BlackTeamFCC
+);
+
+
+
+
+
+
+
+
+
+// The global variable
+var fixedValue = 4;
+
+function incrementer () {
+  // Only change code below this line
+var answer = fixedValue + 1;
+return answer
+
+  // Only change code above this line
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// The global variable
+var fixedValue = 4;
+
+// Only change code below this line
+function incrementer (arg) {
+var arg = 4;
+return arg + 1;
+
+  // Only change code above this line
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// the global variable
+var bookList = ["The Hound of the Baskervilles", "On The Electrodynamics of Moving Bodies", "Philosophiæ Naturalis Principia Mathematica", "Disquisitiones Arithmeticae"];
+
+/* This function should add a book to the list and return the list */
+// New parameters should come before bookName
+
+// Add your code below this line
+function add(arr, bookName) {
+  let newThingy = [...arr]; // Copy the bookList array to a new array.
+  newThingy.push(bookName); // Add bookName parameter to the end of the new array.
+  return newThingy; // Return the new array.
+}
+
+/* This function should remove a book from the list and return the list */
+// New parameters should come before the bookName one
+
+// Add your code below this line
+function remove(arr, bookName) {
+  let newerThingy = [...arr]; // Copy the bookList array to a new array.
+  if (newerThingy.indexOf(bookName) >= 0) {
+    // Check whether the bookName parameter is in new array.
+    newerThingy.splice(newerThingy.indexOf(bookName), 1); // Remove the given paramater from the new array.
+    return newerThingy; // Return the new array.
+  }
+}
+
+var newBookList = add(bookList, 'A Brief History of Time');
+var newerBookList = remove(bookList, 'On The Electrodynamics of Moving Bodies');
+var newestBookList = remove(add(bookList, 'A Brief History of Time'), 'On The Electrodynamics of Moving Bodies');
+
+console.log(bookList);
+
+
+
+
+
+
+
+
+
+
+
+
+const ratings = watchList.map(item => ({
+    title: item["Title"],
+    rating: item["imdbRating"]
+  }));
